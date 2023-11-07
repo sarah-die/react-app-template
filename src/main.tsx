0451fa1218355a102d1bea7bc2c87f2d76c5d048
Sarah Diethert <sarah.diethert@iav.de>
2023-11-07 13:09:34 +0100

    feat: i18next (Internationalization)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// import i18n (needs to be bundled ;))
+import './i18n';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
